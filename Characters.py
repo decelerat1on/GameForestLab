497e0fd992ea773d55a21dede6c0bce023d57720
Deceleration <ohio.ivan@yandex.ru>
2022-10-13 20:07:04 +0300

    Вынос в дату всех данных

--- Characters.py
+++ Characters.py
@@ -24,91 +24,100 @@ class Hero:
             self.dodge = 0.15
             self.critical_damage = 2
             self.chance_critical_damage = 0.05
-
+    def attack(self, target):
+        target.health -= self.damage
 class Enemy:
-    def __init__(self, level, class_enemy):
-        self.level = level
-        self.class_enemy = class_enemy
-        if self.level == 1:
-            if class_enemy == 'Волчья стая':
-                self.health = 250
-                self.damage = 15
-                self.armor = 0
-                self.dodge = 0.03
-                self.critical_damage = 0
-                self.chance_critical_damage = 0
-            elif class_enemy == 'Гоблин с копьем':
-                self.health = 200
-                self.damage = 20
-                self.armor = 1
-                self.dodge = 0.15
-                self.critical_damage = 0
-                self.chance_critical_damage = 0
-            elif class_enemy == 'Иссушенный труп':
-                self.health = 550
-                self.damage = 10
-                self.armor = 1
-                self.dodge = 0
-                self.critical_damage = 0
-                self.chance_critical_damage = 0
-        elif self.level == 2:
-            if class_enemy == 'Странствующий маг':
-                self.health = 400
-                self.damage = 40
-                self.armor = 5
-                self.dodge = 0.1
-                self.critical_damage = 0
-                self.chance_critical_damage = 0
-            elif class_enemy == 'Страж лабиринта':
-                self.health = 600
-                self.damage = 25
-                self.armor = 10
-                self.dodge = 0
-                self.critical_damage = 0
-                self.chance_critical_damage = 0
-            elif class_enemy == 'Душа грешника':
-                self.health = 350
-                self.damage = 35
-                self.armor = 0
-                self.dodge = 0.3
-                self.critical_damage = 2
-                self.chance_critical_damage = 0.2
-        elif self.level == 3:
-            if class_enemy == 'Тролль':
-                self.health = 700
-                self.damage = 30
-                self.armor = 15
-                self.dodge = 0.25
-                self.critical_damage = 0
-                self.chance_critical_damage = 0
-            elif class_enemy == 'Демон лабиринта':
-                self.health = 650
-                self.damage = 60
-                self.armor = 15
-                self.dodge = 0.2
-                self.critical_damage = 0
-                self.chance_critical_damage = 0
-            elif class_enemy == 'Огенный дракон':
-                self.health = 700
-                self.damage = 40
-                self.armor = 20
-                self.dodge = 0.1
-                self.critical_damage = 0
-                self.chance_critical_damage = 0
-        elif self.level == 4:
-            if class_enemy == 'Хранитель лабиринта':
-                self.health = 1000
-                self.damage = 40
-                self.armor = 15
-                self.dodge = 0.18
-                self.critical_damage = 0
-                self.chance_critical_damage = 0
-            elif class_enemy == 'Осколок леса':
-                self.health = 3000
-                self.damage = 0
-                self.armor = 0
-                self.dodge = 0
-                self.critical_damage = 0
-                self.chance_critical_damage = 0
+    def __init__(self, enemy_data):
+        self.name = enemy_data['name']
+        self.health = enemy_data['health']
+        self.damage = enemy_data['damage']
+        self.armor = enemy_data['armor']
+        self.dodge = enemy_data['dodge']
+        self.critical_damage = enemy_data['critical_damage']
+        self.chance_critical_damage = enemy_data['chance_critical_damage']
+
+        # self.level = level
+        # self.class_enemy = class_enemy
+        # if self.level == 1:
+        #     if class_enemy == 'Волчья стая':
+        #         self.health = 250
+        #         self.damage = 15
+        #         self.armor = 0
+        #         self.dodge = 0.03
+        #         self.critical_damage = 0
+        #         self.chance_critical_damage = 0
+        #     elif class_enemy == 'Гоблин с копьем':
+        #         self.health = 200
+        #         self.damage = 20
+        #         self.armor = 1
+        #         self.dodge = 0.15
+        #         self.critical_damage = 0
+        #         self.chance_critical_damage = 0
+        #     elif class_enemy == 'Иссушенный труп':
+        #         self.health = 550
+        #         self.damage = 10
+        #         self.armor = 1
+        #         self.dodge = 0
+        #         self.critical_damage = 0
+        #         self.chance_critical_damage = 0
+        # elif self.level == 2:
+        #     if class_enemy == 'Странствующий маг':
+        #         self.health = 400
+        #         self.damage = 40
+        #         self.armor = 5
+        #         self.dodge = 0.1
+        #         self.critical_damage = 0
+        #         self.chance_critical_damage = 0
+        #     elif class_enemy == 'Страж лабиринта':
+        #         self.health = 600
+        #         self.damage = 25
+        #         self.armor = 10
+        #         self.dodge = 0
+        #         self.critical_damage = 0
+        #         self.chance_critical_damage = 0
+        #     elif class_enemy == 'Душа грешника':
+        #         self.health = 350
+        #         self.damage = 35
+        #         self.armor = 0
+        #         self.dodge = 0.3
+        #         self.critical_damage = 2
+        #         self.chance_critical_damage = 0.2
+        # elif self.level == 3:
+        #     if class_enemy == 'Тролль':
+        #         self.health = 700
+        #         self.damage = 30
+        #         self.armor = 15
+        #         self.dodge = 0.25
+        #         self.critical_damage = 0
+        #         self.chance_critical_damage = 0
+        #     elif class_enemy == 'Демон лабиринта':
+        #         self.health = 650
+        #         self.damage = 60
+        #         self.armor = 15
+        #         self.dodge = 0.2
+        #         self.critical_damage = 0
+        #         self.chance_critical_damage = 0
+        #     elif class_enemy == 'Огенный дракон':
+        #         self.health = 700
+        #         self.damage = 40
+        #         self.armor = 20
+        #         self.dodge = 0.1
+        #         self.critical_damage = 0
+        #         self.chance_critical_damage = 0
+        # elif self.level == 4:
+        #     if class_enemy == 'Хранитель лабиринта':
+        #         self.health = 1000
+        #         self.damage = 40
+        #         self.armor = 15
+        #         self.dodge = 0.18
+        #         self.critical_damage = 0
+        #         self.chance_critical_damage = 0
+        #     elif class_enemy == 'Осколок леса':
+        #         self.health = 3000
+        #         self.damage = 0
+        #         self.armor = 0
+        #         self.dodge = 0
+        #         self.critical_damage = 0
+        #         self.chance_critical_damage = 0
 
 
---
+++ data.py
@@ -0,0 +1,38 @@
+#TODO Дописать лист врагов ( убрать дубликаты )
+#TODO Создать словарь с героями по примеру врагов
+#TODO Перенести данные в дату
+
+lists_enemy = [[{'name':'Волчья стая',
+                'health':250,
+                'damage':15,
+                'armor':0,
+                'dodge':0.03,
+                'critical_damage':0,
+                'chance_critical_damage':0},
+                {'name':'Гоблин с копьем',
+                 'health': 250,
+                 'damage': 15,
+                 'armor': 0,
+                 'dodge': 0.03,
+                 'critical_damage': 0,
+                 'chance_critical_damage': 0
+                 },
+                {'name':'Иссушенный труп',
+                 'health': 250,
+                 'damage': 15,
+                 'armor': 0,
+                 'dodge': 0.03,
+                 'critical_damage': 0,
+                 'chance_critical_damage': 0
+                 }],
+
+               [{'name':'Странствующий маг',},
+                {'name':'Страж лабиринта'},
+                {'name':'Душа грешника'}],
+
+                [{'name':'Тролль',},
+                {'name':'Демон лабиринта'},
+                {'name':'Огенный дракон'}],
+
+                [{'name':'Хранитель лабиринта',},
+                {'name':'Осколок леса'}]]
